d132e5d5777dd8a42bd513dd5c74c92710433190
Lee <39004900+leewayjean@users.noreply.github.com>
2019-11-12 15:56:51 +0800

    Dev: page Food : make menu & food connected active

--- src/views/shop/children/Food.vue
+++ src/views/shop/children/Food.vue
@@ -3,7 +3,13 @@
     <!-- better-scrool 容器 -->
     <section class="categories_wrapper" ref="categories_wrapper">
       <ul class="food_categories">
-        <li v-for="(item,index) in foodList" :key="index" class="categories_item">{{item.name}}</li>
+        <li
+          v-for="(item,index) in foodList"
+          :key="index"
+          class="categories_item"
+          :class="{activeClass:currentIndex == index}"
+          @click="scrollToElement(index)"
+        >{{item.name}}</li>
       </ul>
     </section>
     <!-- better-scrool 容器 -->
@@ -37,25 +43,78 @@ export default {
   data() {
     return {
       foodList: [],
-      id: this.$route.query.id
+      id: this.$route.query.id,
+      listHeight: [], //右边每个li所处高度
+      foodScroll: {}, //右侧的BScroll对象
+      menuScroll: {}, //左侧的BScroll对象
+      scrollY: 0 // Y轴滚动距离
     };
+  },
+  computed: {
+    // 滚动过程中实时计算所在区间索引
+    currentIndex() {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i];
+        let height2 = this.listHeight[i + 1];
+        // 触发滚动事件过程中进行判断，如果滚动过程中处于某个区间，则currentIndex为当前区间的索引
+        if (this.scrollY >= height1 && this.scrollY < height2) {
+          return i;
+        }
+      }
+      return 0;
+    }
+  },
+  methods: {
+    // 点击菜单滚动到某个元素
+    scrollToElement(index) {
+      let elementArray = this.$refs.food_wrapper.getElementsByClassName(
+        "list_item"
+      );
+      this.foodScroll.scrollToElement(elementArray[index], 200);
+    },
+    // 初始化better-scroll
+    initScroll() {
+      this.menuScroll = new BScroll(this.$refs.categories_wrapper, {
+        // ...... 详见配置项
+        click: true
+      });
+      this.foodScroll = new BScroll(this.$refs.food_wrapper, {
+        click: true,
+        // 派发滚动事件
+        probeType: 3
+      });
+    },
+    calculateHeight() {
+      // 获取所有li
+      let itemArray = this.$refs.food_wrapper.getElementsByClassName(
+        "list_item"
+      );
+      let height = 0;
+      let heightArray = [];
+      heightArray.push(height);
+      for (let i = 0; i < itemArray.length; i++) {
+        height += itemArray[i].clientHeight;
+        heightArray.push(height);
+      }
+      this.listHeight = heightArray;
+    }
   },
   created() {
     this.$axios
       .get(`https://elm.cangdu.org/shopping/v2/menu?restaurant_id=${this.id}`)
       .then(res => {
-        console.log(res.data);
         this.foodList = res.data;
       });
+    this.$nextTick(() => {
+      setTimeout(() => {
+        this.initScroll();
+        this.calculateHeight();
+        this.foodScroll.on("scroll", pos => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      }, 3000);
+    });
   },
-  updated() {
-    new BScroll(this.$refs.categories_wrapper, {
-      // ...... 详见配置项
-    });
-    new BScroll(this.$refs.food_wrapper, {
-      // ...... 详见配置项
-    });
-  }
 };
 </script>
 
@@ -85,6 +144,7 @@ export default {
   color: #666;
   background-color: #f5f5f5;
   border-bottom: 1px solid #ddd;
+  border-left: 2px solid #fff;
   /* 单行省略显示 */
   overflow: hidden;
   text-overflow: ellipsis;
@@ -132,5 +192,10 @@ export default {
   font-size: 10px;
   color: #333;
   margin: 5px 0;
+}
+.food .food_categories .categories_item.activeClass {
+  border-left: 2px solid #0089dc;
+  background-color: #fff;
+  font-weight: bold;
 }
 </style>
