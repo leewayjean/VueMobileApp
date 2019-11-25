53d24cb8d8368ab0d01ce1cc5756b03fda973211
Lee <39004900+leewayjean@users.noreply.github.com>
2019-11-25 16:23:01 +0800

    insall & config vant-ui

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,16 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
-  "plugins": [["component", [
-    {
-      "libraryName": "mint-ui",
-      "style": true
-    }
-  ]]]
+  "plugins": [
+    ["component", [
+      {
+        "libraryName": "mint-ui",
+        "style": true
+      }
+    ]],
+    ['import', {
+      libraryName: 'vant',
+      libraryDirectory: 'es',
+      style: true
+    }, 'vant']]
 }
